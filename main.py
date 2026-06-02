094bce4998d103c754418a290601703fe8a9fce0
RemiJolian <rbagheri85@gmail.com>
2026-06-02 10:43:19 +0330

    add comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from smolagents import LiteLLMModel
 
-model = LiteLLMModel(
-    model_id="ollama_chat/gemma3-lmstudio",
-    api_base="http://127.0.0.1:11434",
-    num_ctx=8192,
+# Create a client, to talk to ollama
+model = LiteLLMModel( 
+    model_id="ollama_chat/qwen2:7b", # gemma3-lmstudio or any model you want
+    api_base="http://127.0.0.1:11434", # client talks to this local API
+    num_ctx=8192,  # sets the model's context window to 8,192 tokens
 )
 
-print("Model connected successfully")
+print("Model connected successfully")qwen2:7b 
